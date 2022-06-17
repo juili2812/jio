e9650b1455bb5a8f480678daaaf5abbfe9738a18
juili2812 <jedhejuili6@gmail.com>
2022-06-17 23:31:06 +0530

    My jio committed

---
+++ src/main/java/com/infy/JioApplication.java
@@ -0,0 +1,14 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JioApplication.class, args);
+		System.out.println("My jio project");
+	}
+
+}
